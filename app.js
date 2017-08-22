0f51d26f9d3cf0130da368932f0f2f91224d00f7
Siva Routhu <shivakumarrouthu22@gmail.com>
2017-08-22 22:02:32 +0530

    second commit

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var dust = require('express-dustjs')
+
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -12,7 +14,11 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.engine('dust', dust.engine({
+  // Use dustjs-helpers 
+  useHelpers: true
+}));
+app.set('view engine', 'dust');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render(__dirname + '/public/index.html');
+  res.render('index',{"host":req.host});
 });
 
 module.exports = router;
