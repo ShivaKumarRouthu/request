d747da93e315202f5e435abd6fc0d4d5986533f5
Siva Routhu <shivakumarrouthu22@gmail.com>
2017-08-23 07:26:59 +0530

    bugfix

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+
+router.get('/', function(req, res, next) {
+  res.render('Home Page');
+});
+
 /* GET home page. */
 router.get('/request', function(req, res, next) {
   res.render('index',{"host":req.host});
